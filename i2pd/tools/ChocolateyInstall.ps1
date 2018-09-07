e22a8df9af3084bd4d0ff57d1e1af597a047e91e
R4SAS <r4sas@i2pmail.org>
2018-09-07 05:21:12 +0300

    update i2pd package nuspec and script

--- i2pd/tools/ChocolateyInstall.ps1
+++ i2pd/tools/ChocolateyInstall.ps1
@@ -1,4 +1,29 @@
-﻿
-$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$packagename = 'i2pd'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/PurpleI2P/i2pd/releases/download/2.20.0/i2pd_2.20.0_win32_mingw.zip'
+$checksum = 'AD9A910A50F42598E7425DF137A0FEDFF929E91F095FC284F055F2E4BB38258A'
+$checksumType = 'sha256'
+$url64 = 'https://github.com/PurpleI2P/i2pd/releases/download/2.20.0/i2pd_2.20.0_win64_mingw.zip'
+$checksum64 = 'B819B5F2C390E04C6CB4B5619498B5C0B2E948CEB4FE22FBAF3E0DB3A5EBC422'
+$checksumType64 = 'sha256'
 
-Install-ChocolateyZipPackage -packageName 'i2pd' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/PurpleI2P/i2pd/releases/download/2.20.0/i2pd_2.20.0_win32_mingw.zip' -checksum 'AD9A910A50F42598E7425DF137A0FEDFF929E91F095FC284F055F2E4BB38258A' -checksumType 'sha256'-Url64 'https://github.com/PurpleI2P/i2pd/releases/download/2.20.0/i2pd_2.20.0_win64_mingw.zip' -checksum64 'B819B5F2C390E04C6CB4B5619498B5C0B2E948CEB4FE22FBAF3E0DB3A5EBC422' -checksumType64 'sha256'
+Install-ChocolateyZipPackage -packageName "$packagename" `
+							 -UnzipLocation "$toolsDir" `
+							 -Url "$url" `
+							 -checksum "$checksum" `
+							 -checksumType "$checksumType" `
+							 -Url64 "$url64" `
+							 -checksum64 "$checksum64" `
+							 -checksumType64 "$checksumType64"
+
+# Install configs and certificates if i2pd dir not available, otherwise just update certificates
+$dataDir = Join-Path $Env:APPDATA 'i2pd'
+if (Test-Path -Path $dataDir) {
+	Write-Host "Updating i2pd certificates"
+	Remove-Item $dataDir\certificates -Recurse
+	Copy-Item $toolsDir\contrib\certificates -Destination $dataDir\certificates -Recurse
+} else {
+	Write-Host "Copying default configs and certificates"
+	Copy-Item $toolsDir\contrib -Destination $dataDir -Recurse
+}
