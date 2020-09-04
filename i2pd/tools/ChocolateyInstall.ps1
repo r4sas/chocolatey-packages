0751dcd2b264e1f8c2b16c35c61ac2362f2547f1
R4SAS <r4sas@i2pmail.org>
2020-09-04 09:46:15 +0300

    update i2pd to 2.33.0
    Signed-off-by: R4SAS <r4sas@i2pmail.org>

--- i2pd/tools/ChocolateyInstall.ps1
+++ i2pd/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $packagename = 'i2pd'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/PurpleI2P/i2pd/releases/download/2.32.1/i2pd_2.32.1_win32_mingw.zip'
-$checksum = '387082a81a0eed5f0f95b974fc2e533a78bf8bebaeab2821007ce541a5ee206e'
+$url = 'https://github.com/PurpleI2P/i2pd/releases/download/2.33.0/i2pd_2.33.0_win32_mingw.zip'
+$checksum = 'b25a9ea4674fb637e194d1009ab9ada230e1c5f1bae53f887da5a11597b6e6e8'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/PurpleI2P/i2pd/releases/download/2.32.1/i2pd_2.32.1_win64_mingw.zip'
-$checksum64 = '191341dedba16a63166b1fd92628e8560ecf46ec68f8db51a88288ba6713724a'
+$url64 = 'https://github.com/PurpleI2P/i2pd/releases/download/2.33.0/i2pd_2.33.0_win64_mingw.zip'
+$checksum64 = 'ada894a57de451830dcd50320220233a2606ff255c1031988e7e9c4a463cabde'
 $checksumType64 = 'sha256'
 
 Install-ChocolateyZipPackage -packageName "$packagename" `
