3f51b2ca66615d3de0b7829b1c71eea4fb9ca433
R4SAS <r4sas@i2pmail.org>
2021-02-16 23:52:55 +0300

    update i2pd to 2.36.0
    Signed-off-by: R4SAS <r4sas@i2pmail.org>

--- i2pd/tools/ChocolateyInstall.ps1
+++ i2pd/tools/ChocolateyInstall.ps1
@@ -1,29 +1,29 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $packagename = 'i2pd'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/PurpleI2P/i2pd/releases/download/2.34.0/i2pd_2.34.0_win32_mingw.zip'
-$checksum = 'b22b9a891268b6770b3aef80a1f2780c1e4387f1347b5927ebb04f0e9a965ecc'
+$url = 'https://github.com/PurpleI2P/i2pd/releases/download/2.36.0/i2pd_2.36.0_win32_mingw.zip'
+$checksum = '155209fd53c10b76311dd230ebe017bb7dd12700d7f722c973ec40f8f7e3d854'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/PurpleI2P/i2pd/releases/download/2.34.0/i2pd_2.34.0_win64_mingw.zip'
-$checksum64 = '0ae5d933bca5ac21561b1deef755f4cffa58eb9536dc4c9231ebc212b328795a'
+$url64 = 'https://github.com/PurpleI2P/i2pd/releases/download/2.36.0/i2pd_2.36.0_win64_mingw.zip'
+$checksum64 = '550eddb45f37d2714f6f2e1febf3d41c0b30287f9ed046a8b564ded51787f869'
 $checksumType64 = 'sha256'
 
 Install-ChocolateyZipPackage -packageName "$packagename" `
-							 -UnzipLocation "$toolsDir" `
-							 -Url "$url" `
-							 -checksum "$checksum" `
-							 -checksumType "$checksumType" `
-							 -Url64 "$url64" `
-							 -checksum64 "$checksum64" `
-							 -checksumType64 "$checksumType64"
+                             -UnzipLocation "$toolsDir" `
+                             -Url "$url" `
+                             -checksum "$checksum" `
+                             -checksumType "$checksumType" `
+                             -Url64 "$url64" `
+                             -checksum64 "$checksum64" `
+                             -checksumType64 "$checksumType64"
 
 # Install configs and certificates if i2pd dir not available, otherwise just update certificates
 $dataDir = Join-Path $Env:APPDATA 'i2pd'
 if (Test-Path -Path $dataDir) {
-	Write-Host "Updating i2pd certificates"
-	Remove-Item $dataDir\certificates -Recurse
-	Copy-Item $toolsDir\contrib\certificates -Destination $dataDir\certificates -Recurse
+    Write-Host "Updating i2pd certificates"
+    Remove-Item $dataDir\certificates -Recurse
+    Copy-Item $toolsDir\contrib\certificates -Destination $dataDir\certificates -Recurse
 } else {
-	Write-Host "Copying default configs and certificates"
-	Copy-Item $toolsDir\contrib -Destination $dataDir -Recurse
+    Write-Host "Copying default configs and certificates"
+    Copy-Item $toolsDir\contrib -Destination $dataDir -Recurse
 }
