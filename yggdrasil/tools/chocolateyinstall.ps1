8e527a66ebe52fb6b64ea2e73d4bedaabb3090ee
R4SAS <r4sas@i2pmail.org>
2022-11-21 21:03:06 +0300

    yggdrasil: update to 0.4.7
    Signed-off-by: R4SAS <r4sas@i2pmail.org>

--- yggdrasil/tools/chocolateyinstall.ps1
+++ yggdrasil/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $appDir          = "$([Environment]::GetFolderPath('ProgramFiles'))\Yggdrasil"
 $confDir         = "$([Environment]::GetFolderPath('CommonApplicationData'))\Yggdrasil"
 $startMenuDir    = "$([Environment]::GetFolderPath('CommonStartMenu'))\Programs\Yggdrasil"
 
-$url             = 'https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v0.4.4/yggdrasil-0.4.4-x86.msi'
-$url64           = 'https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v0.4.4/yggdrasil-0.4.4-x64.msi'
-$checksum        = 'd025042823ccaa4909eae9e2cfccfc7ae58b857445b9b4c5f83c533582c2b44f'
-$checksum64      = 'b9b5ee015c9f1ba00e7279cc474aaf139f332c3819f124b22814cb1d5d90f419'
+$url             = 'https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v0.4.7/yggdrasil-0.4.7-x86.msi'
+$url64           = 'https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v0.4.7/yggdrasil-0.4.7-x64.msi'
+$checksum        = '83fe0bb7bb351e13cfcab534aa715e7dd5cf4c2ee8b531c38ee83b47a7403f78'
+$checksum64      = '9882d4d1773a91d26ea32287ccc1d169d0b3751b6db2f165ae5529d6b750da5f'
 
 $packageArgs     = @{
   packageName    = $env:ChocolateyPackageName
